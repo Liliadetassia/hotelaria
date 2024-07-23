c1103d31ff9d4f23988ad68a533eeffc7751e93d
Lilia de Tassia <lylyatavares@gmail.com>
2024-07-23 18:52:03 -0300

    Mensagem descritiva sobre as mudanças

--- conexao.php
+++ conexao.php
@@ -9,6 +9,7 @@ try{
     $pdo = new PDO ("mysql:dbname=$banco;host=$servidor;charset=utf8","$usuario","$senha");
    } catch(Exception $e) {
         echo 'Erro ao conectar ao banco de dados!';
+        echo $e;
 }
 
 
