b06db91f04f08fb2389e3fb16d00cabf89d30bff
Lilia de Tassia <lylyatavares@gmail.com>
2024-07-23 20:30:45 -0300

    validação de usuario e senha

--- autenticar.php
+++ autenticar.php
@@ -7,6 +7,8 @@ require_once("conexao.php");
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
 
-$query = $pdo->query("SELECT * from usuarios");
-
+$query = $pdo->query("SELECT * from usuarios where email = '$usuario'and senha = '$senha'");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$linhas = count($res);
+echo $linhas;
 ?>
