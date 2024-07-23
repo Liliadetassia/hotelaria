7c52d9bc2510522eebb237306a4fb558c98de15b
Lilia de Tassia <lylyatavares@gmail.com>
2024-07-23 19:30:39 -0300

    consulta sql

--- autenticar.php
+++ autenticar.php
@@ -1,5 +1,12 @@
 <?php 
-require_once('conexao.php');
+
+//definir fuso horario
+date_default_timezone_set('America/Sao_Paulo');
+
+require_once("conexao.php");
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
+
+$query = $pdo->query("SELECT * from usuarios");
+
 ?>
