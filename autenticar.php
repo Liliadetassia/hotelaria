7c69401b5ad3434313e1e017730829f8d43cb42c
Lilia de Tassia <lylyatavares@gmail.com>
2024-07-23 21:37:29 -0300

    implantação do metodo prepare

--- autenticar.php
+++ autenticar.php
@@ -7,8 +7,16 @@ require_once("conexao.php");
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
 
-$query = $pdo->query("SELECT * from usuarios where email = '$usuario'and senha = '$senha'");
+$query = $pdo->prepare("SELECT * from usuarios where email = :email and senha = :senha");
+$query->bindValue(":email", "$usuario")
+$query->bindValue(":senha", "$senha")
+$query->execute();
 $res = $query->fetchAll(PDO::FETCH_ASSOC);
 $linhas = count($res);
-echo $linhas;
+
+if($linhas > 0){
+   echo 'Login permitido';
+}
+
+
 ?>
